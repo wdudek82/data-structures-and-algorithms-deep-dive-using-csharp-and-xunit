7c379c8f54ac1a7a817627d40f0c69c76523873e
Wojciech Dudek <wdudek82@gmail.com>
2020-08-21 14:47:50 +0200

    Refactor InsertionSort

--- DataStructuresAndAlgorithmsDeepDive/InsertionSort.cs
+++ DataStructuresAndAlgorithmsDeepDive/InsertionSort.cs
@@ -15,18 +15,17 @@ namespace DataStructuresAndAlgorithmsDeepDive
             for (var i = 1; i < array.Length; i++)
             {
                 var lastUnsortedNum = array[i];
-                var lastCheckedIndex = i;
                 for (var j = i - 1; j >= 0; j--)
                 {
                     var prevNum = array[j];
-                    if (lastUnsortedNum > prevNum)
+
+                    if (prevNum < lastUnsortedNum)
                     {
-                        array[lastCheckedIndex] = lastUnsortedNum;
+                        array[j + 1] = lastUnsortedNum;
                         break;
                     }
 
-                    array[lastCheckedIndex] = prevNum;
-                    lastCheckedIndex = j;
+                    array[j + 1] = prevNum;
                     if (j < 1)
                     {
                         array[j] = lastUnsortedNum;
